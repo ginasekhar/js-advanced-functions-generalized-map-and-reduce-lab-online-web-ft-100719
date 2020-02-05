375a0b1ff9be49e290f4611be43c982474085fcf
Gina Sekhar <ginasekhar@yahoo.com>
2020-02-05 12:57:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map (myArray, myFunction) {
+    let resultArray =  [];
+    for (let i = 0; i < myArray.length; i++)
+    {
+        resultArray.push(myFunction(myArray[i]));
+    }
+    return resultArray;
+}
+
+function reduce (myArray, myFunction, starting_point) {
+    
+    let result = (!!starting_point) ? starting_point : myArray[0]
+    let i = (!!starting_point) ? 0 : 1
+  
+    for (; i < myArray.length; i++)
+    {
+        result = myFunction(myArray[i], result);
+    }
+    return result;
+}
